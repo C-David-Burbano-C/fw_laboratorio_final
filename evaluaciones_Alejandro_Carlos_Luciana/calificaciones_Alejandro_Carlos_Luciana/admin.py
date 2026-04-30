34396993b559d5739d0fa4a7f6ba5b6517d8b6a7
AlejandroEspana <alejandro.espaname@campusucc.edu.co>
2026-04-30 09:12:32 -0500

    mejoras visuales y Styles.css

--- evaluaciones_Alejandro_Carlos_Luciana/calificaciones_Alejandro_Carlos_Luciana/admin.py
+++ evaluaciones_Alejandro_Carlos_Luciana/calificaciones_Alejandro_Carlos_Luciana/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Calificacion
 
-# Register your models here.
+@admin.register(Calificacion)
+class CalificacionAdmin(admin.ModelAdmin):
+    list_display = ('nombre_estudiante', 'identificacion', 'asignatura', 'promedio')
+    search_fields = ('nombre_estudiante', 'identificacion', 'asignatura')
+    readonly_fields = ('promedio',)
